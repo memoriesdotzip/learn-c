2e75f97a277fd672607d78b1ffdfd6b6c28d4ae3
memoriesdotzip <75005882+memoriesdotzip@users.noreply.github.com>
2023-07-16 21:53:19 +0530

    milestone: 1.1 exercises

---
+++ Chapter 1/1.1_GettingStarted.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    /*
+    // Exercise1-1
+    printf("Hello, World!\n");
+    */
+
+   //Exercise1-2
+   printf("Hello, \bWorld!");
+
+   /*
+   Not all characters create valid escape sequences with backslash. Some require suffixes/arguments, e.g., \m, \u, \x. Won't get into details.
+   */
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # learn-c
 c learning stint.
+
+
+---
+
+1. Compiler: gcc
+2. Compile & run: `gcc <name>.c && ./a.out` for UNIX
